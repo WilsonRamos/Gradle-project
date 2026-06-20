8a7c3416ee60641f41832d2748a3ec407b269751
WilsonRamos <developerys@gmail.com>
2026-06-20 16:07:46 -0500

    primeri comit

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -6,7 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import javax.annotation.PostConstruct;
 
-@SpringBootApplication
+@SpringBootApplication @@
 public class App {
 
     public static void main(String[] args)
